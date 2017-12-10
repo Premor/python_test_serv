96642dc76ab25cf4fbfc19bfdf612a4904a97e6e
root <root@diamant-s.ru>
2017-12-10 19:16:37 +0300

    t

--- client.py
+++ client.py
@@ -17,8 +17,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # As you can see, there is no connect() call; UDP has no connections.
 # Instead, data is directly sent to the recipient via sendto().
-sock.sendto(data, (HOST, PORT))
-received = str(sock.recv(1024), "utf-8")
+if sys.argv[1]=='session':
+    sock.sendto(data, (HOST, PORT))
+    received = str(sock.recv(1024), "utf-8")
+    data=bytes('state',"utf-8")
+    sock.sendto(data, (HOST, PORT))
 
 print("Sent:     {}".format(data))
 print("Received: {}".format(received))
