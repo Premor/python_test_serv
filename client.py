9e4ffe1a6e8a1d10df2ee0778f99ff186568d64a
Premor <spamerkik@gmail.com>
2017-12-10 19:15:10 +0300

    working

--- client.py
+++ client.py
@@ -1,24 +1,39 @@
 import socket
 import sys
+import tkinter
+class client:
+    HOST = "diamant-s.ru"  
+    PORT = 9999
+    state=""
+    def create_pack(self,arg):
+        s=""
+        for i in arg:
+            s+=str(i)+","
+        s=s.rstrip(",")
+        return bytes(s,"utf-8")
 
-def create_pack(arg):
-    s=""
-    for i in arg:
-        s+=str(i)+","
-    s=s.rstrip(",")
-    return bytes(s,"utf-8")
+    def session_act(self,event):
+        
+        #data = bytes(";".join(sys.argv[1:]),'utf-8')
+        args=('session',"slave:333;archer:50")
+        data=self.create_pack(args)
+        #data=bytes()
+    # SOCK_DGRAM is the socket type to use for UDP sockets
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-HOST, PORT = "diamant-s.ru", 9999
-data = bytes(",".join(sys.argv[1:]),'utf-8')
-#args=('session',1,1)
-#data=create_pack(args)
-# SOCK_DGRAM is the socket type to use for UDP sockets
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # As you can see, there is no connect() call; UDP has no connections.
+    # Instead, data is directly sent to the recipient via sendto().
+        sock.sendto(data, (self.HOST, self.PORT))
+        received = str(sock.recv(1024), "utf-8")
+        self.state['text']=received
+    def __init__(self):
+        root = tkinter.Tk()
+        btn = tkinter.Button(root,text="Click me",width=30,height=5)
+        lab= tkinter.Label(root,text="state")
+        self.state=lab
+        lab.pack()
+        btn.bind("<Button-1>",self.session_act)
+        btn.pack()
+        root.mainloop()
 
-# As you can see, there is no connect() call; UDP has no connections.
-# Instead, data is directly sent to the recipient via sendto().
-sock.sendto(data, (HOST, PORT))
-received = str(sock.recv(1024), "utf-8")
-
-print("Sent:     {}".format(data))
-print("Received: {}".format(received))
+a=client()
