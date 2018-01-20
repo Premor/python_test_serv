7b159cf17f6560e2578b33d7dd6a254b47d93c86
Premor <spamerkik@gmail.com>
2018-01-21 01:10:22 +0300

    плитка готова

--- client2.py
+++ client2.py
@@ -20,7 +20,45 @@ class cell:
         re=pygame.Rect(hexg[4][0],hexg[4][1],abs(hexg[1][0]-hexg[2][0]),abs(hexg[2][1]-hexg[4][1]))
         re2=pygame.Rect(hexg[1][0],hexg[1][1]-r2/2,r/4,r2)
         re3=pygame.Rect(hexg[4][0],hexg[4][1]+r2/2,-r/4,r2)
+        self.rect_list=[re,re2,re3]
 
+def map_generate():
+    maps=[]
+    adj=[]
+    buf_cell=cell(50,50,50)
+    for i in range(0,8):
+        maps.append(list())
+        for j in range(0,8):
+            if i%2==0:
+                maps[i].append(cell(50+j*buf_cell.r*3,50+i*buf_cell.r2,50))
+            else:
+                maps[i].append(cell(50+(buf_cell.r+buf_cell.r/2)+j*buf_cell.r*3,50+i*buf_cell.r2,50))
+    
+    for i in range(0,8):
+        adj.append(list())
+        for j in range(0,8):
+            adj[i].append(list())
+            if i+1 in range(0,8):
+                adj[i][j].append([i+1,j])
+                if i%2==0:
+                    if j-1 in range(0,8):
+                        adj[i][j].append([i+1,j-1])
+                else:
+                    if j+1 in range(0,8):
+                        adj[i][j].append([i+1,j+1])
+            if i-1 in range(0,8):
+                adj[i][j].append([i-1,j])
+                if i%2==0:
+                    if j-1 in range(0,8):
+                        adj[i][j].append([i-1,j-1])
+                else:
+                    if j+1 in range(0,8):
+                        adj[i][j].append([i-1,j+1])
+            if j+1 in range(0,8):
+                adj[i][j].append([i,j+1])
+            if j-1 in range(0,8):
+                adj[i][j].append([i,j-1])
+    return maps,adj
 
 
 def hexagon(x,y,r):
@@ -38,7 +76,7 @@ def hexagon(x,y,r):
 
 pygame.init()
 update_list=[]
-screen = pygame.display.set_mode((640, 480))
+screen = pygame.display.set_mode((800, 600))
 #background = pygame.image.load('1.png').convert()
 hexg=hexagon(100,100,50)
 r=50
@@ -50,7 +88,10 @@ print(hexg)
 print(re.topleft, re.bottomleft, re.topright, re.bottomright)
 screen.fill(pygame.Color(255,255,255,255))
 #screen.blit(background, (0, 0))
-pygame.draw.polygon(screen,pygame.Color(0,0,0,255),hexg,1)
+maps,adj = map_generate()
+for i in maps:
+    for j in i:
+        pygame.draw.polygon(screen,pygame.Color(0,0,0,255),j.list_points,1)
 #pygame.draw.rect(screen,pygame.Color(255,0,0,255),re,2)
 #pygame.draw.rect(screen,pygame.Color(255,0,0,255),re2,2)
 #pygame.draw.rect(screen,pygame.Color(255,0,0,255),re3,2)
